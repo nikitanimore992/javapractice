f4b220e65fdd3848b53a56be6712b5296a4fe5b0
nikitanimore992 <992nikitanimore@gmail.com>
2026-03-07 17:17:31 +0530

    constructor create

---
+++ src/main/java/com/mycompany/main/constructor.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.main;
+
+/**
+ *
+ * @author pramo
+ */
+class person{
+    String name;
+    int age;
+    
+//    person(){
+//    //  non parametorized constructor
+//       name = "Nikita";
+//       age = 24;
+//    }
+//  parameterized construstor
+    
+   person(String name, int age){
+       this.name=name; 
+       this.age=age;
+   }
+    
+    void output(){
+        System.out.println("Name = "+name);
+        System.out.println("Age = "+age);
+    }
+}
+public class constructor {
+    public static void main(String[] args) {
+    person person1= new person("Nikita", 24);
+    person1.output();
+    
+        
+    }
+}
