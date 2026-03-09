7e36a55217a9ff71c6217c91fd3dbc2356e65acc
nikitanimore992 <992nikitanimore@gmail.com>
2026-03-09 16:08:54 +0530

    practice for encapsulation public and private access modifire

---
+++ src/main/java/com/mycompany/main/Encapsulationprivate.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.main;
+
+/**
+ *
+ * @author pramo
+ */
+
+class Hum {
+
+    private String name;
+    private int age;
+
+    // setter
+    void setValue(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // getter / display
+    void getValue() {
+        System.out.println("Name is " + name);
+        System.out.println("Age is " + age);
+    }
+}
+
+public class Encapsulationprivate {
+
+    public static void main(String[] args) {
+
+        Hum h = new Hum();
+        
+        h.setValue("Nikita", 24);
+        h.getValue();
+         h.setValue("yogita", 22);
+        h.getValue();
+    }
+}
---
+++ src/main/java/com/mycompany/main/Encapsulationspublic.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.main;
+
+/**
+ *
+ * @author pramo
+ */
+class Human{
+    public int age;
+    void output(){
+        System.out.println("Age is "+age);
+    }
+}
+
+public class Encapsulationspublic {
+    public static void main(String[] args) {
+        Human nikky = new Human();
+        nikky.age = 24;
+        nikky.output();
+    }
+}
