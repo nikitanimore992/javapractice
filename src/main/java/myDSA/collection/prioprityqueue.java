0cbd1e05a82bf94084fbabccc3e145cf836c621c
nikitanimore992 <992nikitanimore@gmail.com>
2026-03-17 15:34:35 +0530

    priorityqueue

--- src/main/java/myDSA/collection/prioprityqueue.java
+++ src/main/java/myDSA/collection/prioprityqueue.java
@@ -31,6 +31,21 @@ public class prioprityqueue {
          
          System.out.println(queue.peek());
          
+         // sometime we want to perform operation on the  element from last like reverse order 
+         
+         // implement a new priority queue
+         Queue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+         
+        pq.offer(20);
+        pq.offer(10);
+        pq.offer(50);
+        pq.offer(30); 
+        pq.offer(40); 
+        System.out.println(pq);
+        pq.poll();
+        System.out.println(pq);
+        System.out.println(pq.peek());
+        
          
     }
 }
