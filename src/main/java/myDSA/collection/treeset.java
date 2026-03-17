6f102e2b984877f706222d8b1fe6d9e0898ad658
nikitanimore992 <992nikitanimore@gmail.com>
2026-03-17 22:58:00 +0530

    create a tree set

---
+++ src/main/java/myDSA/collection/treeset.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package myDSA.collection;
+
+
+import java.util.*;
+
+/**
+ *
+ * @author pramo
+ */
+public class treeset {
+    public static void main(String[] args) {
+         Set<Integer> set = new TreeSet<>();
+        
+        set.add(10);
+        set.add(20);
+        set.add(30);
+        set.add(40);
+        set.add(50);
+        set.add(60);
+        System.out.println(set);
+        
+        // remove element from hashset
+        set.remove(40);
+        System.out.println(set);
+        
+        // chack if particaler element is available in hashset or not
+        System.out.println(set.contains(40)); // it returns true or false
+        System.out.println(set.contains(50));
+        
+        // check hashset is empty
+        System.out.println( set.isEmpty()); // it return true
+        
+        // check size of hashset
+        System.out.println(set.size());
+        
+        // if i want to clear all element in hashset
+        System.out.println(set);
+        set.clear();
+        System.out.println(set);
+    }
+}
